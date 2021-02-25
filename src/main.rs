7ee0af35862889de333faed5f6cd6d97cd3bd350
Hjalte Nannestad <hjalte.nannestad@gmail.com>
2021-02-25 21:01:35 +0100

    updated tuning functions

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,7 @@
 pub mod algoritm;
 
 use algoritm::*;
+use std::fmt::{Display, Formatter};
 
 #[derive(Copy, Clone, PartialEq, Eq, Debug)]
 enum Colour {
@@ -10,6 +11,21 @@ enum Colour {
     White,
     Red,
     Orange,
+}
+
+impl Display for Colour {
+    fn fmt(&self, f: &mut Formatter) -> std::fmt::Result {
+        match self {
+            Self::Green => write!(f, "G")?,
+            Self::Blue => write!(f, "B")?,
+            Self::Yellow => write!(f, "Y")?,
+            Self::White => write!(f, "W")?,
+            Self::Red => write!(f, "R")?,
+            Self::Orange => write!(f, "O")?,
+        }
+
+        Ok(())
+    }
 }
 
 #[derive(Clone, PartialEq, Eq, Debug)]
@@ -23,6 +39,18 @@ struct Face {
     bl: Colour,
     bm: Colour,
     br: Colour,
+}
+
+impl Display for Face {
+    fn fmt(&self, f: &mut Formatter) -> std::fmt::Result {
+        writeln!(f, "{}|{}|{}", self.ul, self.um, self.ur)?;
+        writeln!(f, "-+-+-")?;
+        writeln!(f, "{}|{}|{}", self.ml, self.mm, self.mr)?;
+        writeln!(f, "-+-+-");
+        writeln!(f, "{}|{}|{}", self.bl, self.bm, self.br)?;
+
+        Ok(())
+    }
 }
 
 /// Ordering of contents of lines are in clockwise order
@@ -115,6 +143,87 @@ struct Cube {
     down: Face,
 }
 
+impl Display for Cube {
+    fn fmt(&self, f: &mut Formatter) -> std::fmt::Result {
+        writeln!(f, "      {}|{}|{}", self.up.ul, self.up.um, self.up.ur);
+        writeln!(f, "      -+-+-");
+        writeln!(f, "      {}|{}|{}", self.up.ml, self.up.mm, self.up.mr);
+        writeln!(f, "      -+-+-");
+        writeln!(f, "      {}|{}|{}", self.up.bl, self.up.bm, self.up.br);
+        writeln!(f, "");
+        writeln!(
+            f,
+            "{}|{}|{} {}|{}|{} {}|{}|{} {}|{}|{}",
+            self.left.ul,
+            self.left.um,
+            self.left.ur,
+            self.front.ul,
+            self.front.um,
+            self.front.ur,
+            self.right.ul,
+            self.right.um,
+            self.right.ur,
+            self.back.ul,
+            self.back.um,
+            self.back.ur
+        );
+        writeln!(f, "-+-+- -+-+- -+-+- -+-+-");
+        writeln!(
+            f,
+            "{}|{}|{} {}|{}|{} {}|{}|{} {}|{}|{}",
+            self.left.ml,
+            self.left.mm,
+            self.left.mr,
+            self.front.ml,
+            self.front.mm,
+            self.front.mr,
+            self.right.ml,
+            self.right.mm,
+            self.right.mr,
+            self.back.ml,
+            self.back.mm,
+            self.back.mr
+        );
+        writeln!(f, "-+-+- -+-+- -+-+- -+-+-");
+        writeln!(
+            f,
+            "{}|{}|{} {}|{}|{} {}|{}|{} {}|{}|{}",
+            self.left.bl,
+            self.left.bm,
+            self.left.br,
+            self.front.bl,
+            self.front.bm,
+            self.front.br,
+            self.right.bl,
+            self.right.bm,
+            self.right.br,
+            self.back.bl,
+            self.back.bm,
+            self.back.br
+        );
+        writeln!(f, "");
+        writeln!(
+            f,
+            "      {}|{}|{}",
+            self.down.ul, self.down.um, self.down.ur
+        );
+        writeln!(f, "      -+-+-");
+        writeln!(
+            f,
+            "      {}|{}|{}",
+            self.down.ml, self.down.mm, self.down.mr
+        );
+        writeln!(f, "      -+-+-");
+        writeln!(
+            f,
+            "      {}|{}|{}",
+            self.down.bl, self.down.bm, self.down.br
+        );
+
+        Ok(())
+    }
+}
+
 impl Default for Cube {
     fn default() -> Self {
         Cube {
@@ -129,6 +238,175 @@ impl Default for Cube {
 }
 
 impl Cube {
+    pub fn get_face(&self, side: Side) -> &Face {
+        match side {
+            Side::Up => &self.up,
+            Side::Down => &self.down,
+            Side::Front => &self.front,
+            Side::Back => &self.back,
+            Side::Right => &self.right,
+            Side::Left => &self.left,
+        }
+    }
+
+    pub fn get_face_mut(&mut self, side: Side) -> &mut Face {
+        match side {
+            Side::Up => &mut self.up,
+            Side::Down => &mut self.down,
+            Side::Front => &mut self.front,
+            Side::Back => &mut self.back,
+            Side::Right => &mut self.right,
+            Side::Left => &mut self.left,
+        }
+    }
+
+    pub fn get_up_lines(&self) -> [Line; 4] {
+        [
+            self.front.get_upper_line(),
+            self.left.get_upper_line(),
+            self.back.get_upper_line(),
+            self.right.get_upper_line(),
+        ]
+    }
+
+    pub fn get_down_lines(&self) -> [Line; 4] {
+        [
+            self.front.get_bottom_line(),
+            self.left.get_bottom_line(),
+            self.back.get_bottom_line(),
+            self.right.get_bottom_line(),
+        ]
+    }
+
+    pub fn get_front_lines(&self) -> [Line; 4] {
+        [
+            self.up.get_bottom_line(),
+            self.right.get_left_line(),
+            self.down.get_upper_line(),
+            self.left.get_right_line(),
+        ]
+    }
+
+    pub fn get_back_lines(&self) -> [Line; 4] {
+        [
+            self.up.get_upper_line(),
+            self.right.get_right_line(),
+            self.down.get_bottom_line(),
+            self.left.get_left_line(),
+        ]
+    }
+
+    pub fn get_right_lines(&self) -> [Line; 4] {
+        [
+            self.front.get_right_line(),
+            self.up.get_right_line(),
+            self.back.get_left_line(),
+            self.down.get_right_line(),
+        ]
+    }
+
+    pub fn get_left_lines(&self) -> [Line; 4] {
+        [
+            self.front.get_left_line(),
+            self.up.get_left_line(),
+            self.back.get_right_line(),
+            self.down.get_left_line(),
+        ]
+    }
+
+    pub fn set_up_lines(&mut self, [a, b, c, d]: [Line; 4]) {
+        self.front.set_upper_line(a);
+        self.left.set_upper_line(b);
+        self.back.set_upper_line(c);
+        self.right.set_upper_line(d);
+    }
+
+    pub fn set_down_lines(&mut self, [a, b, c, d]: [Line; 4]) {
+        self.front.set_bottom_line(a);
+        self.left.set_bottom_line(b);
+        self.back.set_bottom_line(c);
+        self.right.set_bottom_line(d);
+    }
+
+    pub fn set_front_lines(&mut self, [a, b, c, d]: [Line; 4]) {
+        self.front.set_bottom_line(a);
+        self.right.set_left_line(b);
+        self.down.set_upper_line(c);
+        self.left.set_right_line(d);
+    }
+
+    pub fn set_back_lines(&mut self, [a, b, c, d]: [Line; 4]) {
+        self.front.set_upper_line(a);
+        self.right.set_right_line(b);
+        self.down.set_bottom_line(c);
+        self.left.set_left_line(d);
+    }
+
+    pub fn set_right_lines(&mut self, [a, b, c, d]: [Line; 4]) {
+        self.front.set_right_line(a);
+        self.up.set_right_line(b);
+        self.back.set_left_line(c);
+        self.down.set_right_line(d);
+    }
+
+    pub fn set_left_lines(&mut self, [a, b, c, d]: [Line; 4]) {
+        self.front.set_left_line(a);
+        self.up.set_left_line(b);
+        self.back.set_right_line(c);
+        self.down.set_left_line(d);
+    }
+
+    pub fn get_lines(&self, side: Side) -> [Line; 4] {
+        match side {
+            Side::Up => self.get_up_lines(),
+            Side::Down => self.get_down_lines(),
+            Side::Front => self.get_front_lines(),
+            Side::Back => self.get_back_lines(),
+            Side::Right => self.get_right_lines(),
+            Side::Left => self.get_left_lines(),
+        }
+    }
+
+    pub fn set_lines(&mut self, side: Side, lines: [Line; 4]) {
+        match side {
+            Side::Up => self.set_up_lines(lines),
+            Side::Down => self.set_down_lines(lines),
+            Side::Front => self.set_front_lines(lines),
+            Side::Back => self.set_back_lines(lines),
+            Side::Right => self.set_right_lines(lines),
+            Side::Left => self.set_left_lines(lines),
+        }
+    }
+
+    pub fn turn_side(&mut self, side: Side) {
+        self.get_face_mut(side.clone()).rot_clockwise();
+
+        let mut lines = self.get_lines(side.clone());
+        lines.rotate_right(1);
+        self.set_lines(side, lines);
+    }
+
+    pub fn turn_side_prime(&mut self, side: Side) {
+        self.get_face_mut(side.clone()).rot_counterclockwise();
+
+        let mut lines = self.get_lines(side.clone());
+        lines.rotate_left(1);
+        self.set_lines(side, lines);
+    }
+
+    pub fn turn_side_twice(&mut self, side: Side) {
+        let face = self.get_face_mut(side.clone());
+        face.rot_clockwise();
+        face.rot_clockwise();
+
+        let mut lines = self.get_lines(side.clone());
+        lines.rotate_right(2);
+        self.set_lines(side, lines);
+    }
+
+    // these functions shouldn't be needed, review before removing
+    //      Hjalte 25-02-21
+    /*
     pub fn turn_right(&mut self) {
         self.right.rot_clockwise();
         let front_right_line: Line = self.front.get_right_line();
@@ -272,39 +550,13 @@ impl Cube {
         self.back.set_upper_line(right_upper_line);
         self.right.set_upper_line(front_upper_line);
     }
+    */
 
     pub fn execute_move(&mut self, mv: Move) {
-        match &mv.side {
-            Side::Up => match &mv.ty {
-                MoveType::Normal => self.turn_up(),
-                MoveType::Prime => self.turn_up_prime(),
-                MoveType::Double => unimplemented!(),
-            },
-            Side::Down => match mv.ty {
-                MoveType::Normal => self.turn_down(),
-                MoveType::Prime => self.turn_down_prime(),
-                MoveType::Double => unimplemented!(),
-            },
-            Side::Right => match mv.ty {
-                MoveType::Normal => self.turn_right(),
-                MoveType::Prime => self.turn_right_prime(),
-                MoveType::Double => unimplemented!(),
-            },
-            Side::Left => match mv.ty {
-                MoveType::Normal => self.turn_left(),
-                MoveType::Prime => self.turn_left_prime(),
-                MoveType::Double => unimplemented!(),
-            },
-            Side::Front => match mv.ty {
-                MoveType::Normal => self.turn_front(),
-                MoveType::Prime => self.turn_front_prime(),
-                MoveType::Double => unimplemented!(),
-            },
-            Side::Back => match mv.ty {
-                MoveType::Normal => self.turn_back(),
-                MoveType::Prime => self.turn_back_prime(),
-                MoveType::Double => unimplemented!(),
-            },
+        match &mv.ty {
+            MoveType::Normal => self.turn_side(mv.side.clone()),
+            MoveType::Prime => self.turn_side_prime(mv.side.clone()),
+            MoveType::Double => self.turn_side_twice(mv.side.clone()),
         }
     }
 
@@ -315,7 +567,19 @@ impl Cube {
     }
 }
 
-fn main() {}
+fn main() {
+    let mut cube = Cube::default();
+
+    println!("{}", cube);
+
+    let alg = Algorithm::parse("R U R' U'").unwrap();
+
+    println!("{:?}", alg);
+
+    cube.execute_algorithm(alg);
+
+    println!("{}", cube);
+}
 
 #[cfg(test)]
 mod tests {
